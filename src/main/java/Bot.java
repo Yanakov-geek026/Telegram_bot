d1e8a2336c5d8c14c7333ec96876c314885a0b43
16000191 <yanakov_gasu@mail.ru>
2019-10-22 17:37:07 +0300

    It works, maybe;

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,9 +4,19 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+
+import static jdk.nashorn.internal.runtime.ECMAErrors.getMessage;
+
 public class Bot extends TelegramLongPollingBot {
+
+    private long chat_id;
+    String lastMassege = "";
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
     public Bot(DefaultBotOptions botOptions) {
         super(botOptions);
@@ -14,9 +24,15 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        SendMessage sendMessage = new SendMessage(message.getChatId(), message.getText());
+        update.getUpdateId();
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        chat_id = update.getMessage().getChatId();
+
+        String text = update.getMessage().getText();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
         try {
+            sendMessage.setText(getMessage(text));
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
@@ -31,5 +47,29 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "908393887:AAHu8SKsnTtlBOsxaJ-7Fsjkdmp-CZ8mpVM";
+    }
+
+    public String getMessage(String msg) {
+        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        if (msg.equals("Hello") || msg.equals("Menu")) {
+            keyboard.clear();
+            keyboardFirstRow.clear();
+            keyboardFirstRow.add("Popular");
+            keyboardFirstRow.add("News");
+            keyboardFirstRow.add("Helpful information");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Select...";
+        } else {
+            return null;
+        }
     }
 }
